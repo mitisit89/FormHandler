e1d86d8506947459c3372bc1333460dc0de3e7f1
mitisit <mitisit89@gmail.com>
2023-01-07 07:48:08 +0300

    validate works

--- app.py
+++ app.py
@@ -3,46 +3,57 @@ import re
 
 import uvicorn
 from fastapi import FastAPI, Query
-from pydantic import (BaseModel, EmailStr, ValidationError, validate_email,
-                      validator)
-from tinydb import Query as db_q
-from tinydb import TinyDB
+from tinydb import Query as db_query
+from tinydb import TinyDB, where
 
 app = FastAPI()
 db = TinyDB("db.json")
+forms = db.table("forms")
 
 
-class DateValidator(BaseModel):
-    date: datetime.date
-
-    @validator("date", pre=True)
-    def parse_birthdate(cls, value):
-        return datetime.datetime.strptime(value, "%Y-%m-%d").date()
+def date_validator(d: datetime) -> str:
+    if datetime.datetime.strptime(d, "%Y-%m-%d").date():
+        return "date"
+    return
 
 
-class PhoneValidator(BaseModel):
-    phone: str
-
-    @validator("phone")
-    def phone_validator(cls, value):
-        regex = r"^(\+)[1-9][0-9\-\(\)\.]{9,15}$"
-        if value and not re.search(regex, value, re.I):
-            raise ValueError("Phone Number Invalid.")
-        return value
+def phone_validator(phone: str) -> str:
+    regex = r"^(\+)[1-9][0-9\-\(\)\.]{9,15}$"
+    if phone and not re.search(regex, phone, re.I):
+        print("Phone Number Invalid.")
+    return "phone"
 
 
-@app.post("/get_form")
+def mail_validator(email: str) -> str:
+    regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    if re.fullmatch(regex, email):
+        return "email"
+
+
+def validator(form: dict) -> dict:
+    __validate_form = {}
+    for key, value in form.items():
+        if value.startswith("+"):
+            __validate_form[key] = phone_validator(value)
+        elif "@" in value:
+            __validate_form[key] = mail_validator(value)
+
+        elif "-" in value:
+            __validate_form[key] = date_validator(value)
+        else:
+            __validate_form[key] = "text"
+    return __validate_form
+
+
+@app.post("/get_form", status_code=201)
 async def get_form(query: list[str] = Query(default=[])):
-    print(type(query[2].split("=")[1]))
-    fields = {i.split("=")[0]: i.split("=")[1] for i in query}
-    if (
-        validate_email(query[0].split("=")[1])
-        and DateValidator(date=query[2].split("=")[1])
-        and PhoneValidator(phone=query[1].split("=")[1])
-    ):
-        print("email data phone")
+    form = {i.split("=")[0]: i.split("=")[1] for i in query}
+    validate_form = validator(form)
+    if form_name := forms.search(db_query().fragment(validate_form)):
 
-    return 201
+        return form_name[0].get("name")
+    else:
+        return validate_form
 
 
 if __name__ == "__main__":
